b15ecc485942e903474c9122bfff9634fbe0cfa3
kathelin monique <kathelin.silva@eaportal.org>
2024-09-01 02:42:11 +0000

    metade do js das questões do terceiro ano completos

--- Site Mathtech/questões(bc.exercicio e quiz)/terceiroano.js
+++ Site Mathtech/questões(bc.exercicio e quiz)/terceiroano.js
@@ -120,5 +120,363 @@ export const exercicios = [
                 "answer": "log am = m log a"
             },
         ]
+    },
+
+    {
+        "title": "trigonometria (3º ano)",
+        "questions": [
+            {
+                "question": " (Qconcursos 2024) Em um triângulo ABC, o ângulo A mede 70°. O ângulo formado pelas bissetrizes dos outros dois ângulos (B e C) é:",
+                "options": [
+                    "110°", 
+                    "125°",
+                    "145°",
+                    "55°",
+                    "90°"
+                ],
+                "answer": "125°"
+            },
+
+            {
+                "question": "(UFG 2024) Em um triângulo retângulo, os ângulos internos medem 35° e 55°. O lado oposto ao ângulo de 35° mede x e o lado oposto ao ângulo de 55° mede y. Qual o valor de sen2 (35°) + sen2 (55°)? ",
+                "options": [
+                    "1", 
+                    "2",
+                    "3",
+                    "4",
+                    "5"
+                ],
+                "answer": "1"
+            },
+
+            {
+                "question": "(FUNDATEC 2024)Considerando que senx = 3/5 para π/2 < x < π, qual é o valor de cotgx?",
+                "options": [
+                    "− 4/3", 
+                    "− 3/4",
+                    "1",
+                    "3/4",
+                    "4/3"
+                ],
+                "answer": "− 4/3"
+            },
+
+            {
+                "question": "(FEPESE 2021) Se x é um ângulo tal que 2π​ ≤ x ≤ π e sen (x) = 33​​ , então tan(x) é igual a:",
+                "options": [
+                    "2", 
+                    "22",
+                    "−23",
+                    "−22​​",
+                    "−2"
+                ],
+                "answer": "−22"
+            },
+
+            {
+                "question": "(UFG 2024) Considerando as duas funções f(x) = x, e g(x) = sen(x), para x em radianos, ambas restritas ao intervalo [0, π/2], qual o maior valor da soma s(x) = f(x) + g(x)? ",
+                "options": [
+                    "(2π + 1)/2", 
+                    "(π + 2)/2",
+                    "(2π -1)/2",
+                    "(π - 1)/2"
+                ],
+                "answer": "(π + 2)/2"
+            },
+
+            {
+                "question": "(IBADE 2024) Quais são os valores de tanθ obtidos da equação 3(sec2 θ – 1) + 16 tan θ + 5 = 0?",
+                "options": [
+                    "{ 1/2 , -1/4 }", 
+                    "{ 1/7 , 6 }",
+                    "{ - 2/7 , -2 }",
+                    "{ -1/4 , 3 }",
+                    "{ -1/3 , -5 }"
+                ],
+                "answer": "{ -1/3 , -5 }"
+            },
+
+            {
+                "question": "(IPEFAE 2024) Ensinando trigonometria, o professor Pedro solicitou para que seus estudantes para que calculassem o valor do cosseno do ângulo β sabendo que ele está no segundo quadrante e equivale à 180º - α. Trouxe, também, a informação que o sen α = 0,4. Qual o valor que ele espera que seus estudantes encontrem é aproximadamente quanto?",
+                "options": [
+                    "- 0,9", 
+                    "- 0,84",
+                    "0,16",
+                    "0,91"
+                ],
+                "answer": "- 0,9"
+            },
+
+            {
+                "question": "(Itame 2023) Em relação a circunferência cuja equação é dada por x2 + y2 + 4x − 6y − 23 = 0, podemos dizer que o ponto P(2,0)",
+                "options": [
+                    "pertence a circunferência.", 
+                    "é exterior a circunferência",
+                    "é interno a circunferência. ",
+                    "é o centro da circunferência. "
+                ],
+                "answer": "é interno a circunferência. "
+            },
+
+            {
+                "question": "(IDHTEC 2023) Seja n um número natural par tal que a sequência S = sen x + sen2x + sen3x +sen4x + ⋯ + sennx, com x ≠ π / 2 + kπ e k ∈ ℤ. Se há uma expressão que determina S, essa expressão pode ser:",
+                "options": [
+                    "(sen x) n+1 - sen x / sen x - 1", 
+                    "( 1 + sen x ) . ( 1 + sen n x)",
+                    "sen x + sen n x",
+                    "( 1 + sen x ) n2 + n / 8",
+                    "(senx) n / 4 + (sen x) n2 / 8"
+                ],
+                "answer": "(sen x) n+1 - sen x / sen x - 1"
+            },
+
+            {
+                "question": "Certa universidade fez um estudo com 1000 pessoas. A mesma fez uma análise dos batimentos cardíacos desses indivíduos em um determinado período. Após esse estudo, criou-se um modelo matemático para representar os batimentos cardíacos desse grupo de pessoas, cuja lei de formação é dada por P(t) = 80 – 18 cos Imagem associada para resolução da questão, tal que 0 < t ≤ 0,6 e t é dado em segundos. Com base nesses dados é possível afirmar que o maior valor da pressão arterial atingida pelos integrantes desse grupo ocorre quando t é igual a:",
+                "options": [
+                    "0,1 segundos", 
+                    "0,2 segundos",
+                    "0,25 segundos",
+                    "0,3 segundos"
+                ],
+                "answer": "0,3 segundos"
+            },
+        ]
+    },
+
+    {
+        "title": "probabilidade (3º ano)",
+        "questions": [
+            {
+                "question": "(Fuvest) Uma urna contém 5 bolas brancas e 3 bolas pretas. Três bolas são retiradas ao acaso, sucessivamente, sem reposição. Determine:",
+                "options": [
+                    "A probabilidade de que tenham sido retiradas 2 bolas pretas e 1 bola branca.", 
+                    "A probabilidade de que tenham sido retiradas 2 bolas pretas e 1 bola branca, sabendo-se que as três bolas não são da mesma cor."
+                ],
+                "answer": "A probabilidade de que tenham sido retiradas 2 bolas pretas e 1 bola branca."
+            },
+
+            {
+                "question": "(Enem) Numa escola com 1.200 alunos foi realizada uma pesquisa sobre o conhecimento desses em duas línguas estrangeiras, inglês e espanhol. Nessa pesquisa constatou-se que 600 alunos falam inglês, 500 falam espanhol e 300 não falam qualquer um desses idiomas. Escolhendo-se um aluno dessa escola ao acaso e sabendo-se que ele não fala inglês, qual a probabilidade de que esse aluno fale espanhol?",
+                "options": [
+                    "1/2", 
+                    "5/8",
+                    "1/4",
+                    "5/6",
+                    "5/14"
+                ],
+                "answer": "1/2"
+            },
+
+            {
+                "question": "(Enem) Para analisar o desempenho de um método diagnóstico, realizam-se estudos em populações contendo pacientes sadios e doentes. Quatro situações distintas podem acontecer nesse contexto de teste:  1) Paciente TEM a doença e o resultado do teste é POSITIVO. 2) Paciente TEM a doença e o resultado do teste é NEGATIVO. 3) Paciente NÃO TEM a doença e o resultado do teste é POSITIVO. 4) Paciente NÃO TEM a doença e o resultado do teste é NEGATIVO. Um índice de desempenho para avaliação de um teste de diagnóstico é a sensibilidade, definida como a probabilidade de o resultado do teste ser POSITIVO se o paciente estiver com a doença. O quadro refere-se a um teste diagnóstico para a doença A, aplicado em uma amostra composta por duzentos indivíduos. Conforme o quadro teste proposto, a sensibilidade dele é de:",
+                "options": [
+                    "47,5%.", 
+                    "85,0%.",
+                    "86,3%.",
+                    "94,4%.",
+                    "95,0%."
+                ],
+                "answer": "95,0%."
+            },
+
+            {
+                "question": "(Enem) O diretor de uma escola convidou os 280 alunos de terceiro ano a participarem de uma brincadeira. Suponha que existem 5 objetos e 6 personagens numa casa de 9 cômodos; um dos personagens esconde um dos objetos em um dos cômodos da casa. O objetivo da brincadeira é adivinhar qual objeto foi escondido, por qual personagem e em qual cômodo da casa o objeto foi escondido. Todos os alunos decidiram participar. A cada vez um aluno é sorteado e dá a sua resposta. As respostas devem ser sempre distintas das anteriores, e um mesmo aluno não pode ser sorteado mais de uma vez. Se a resposta do aluno estiver correta, ele é declarado vencedor e a brincadeira é encerrada. O diretor sabe que algum aluno acertará a resposta porque há:",
+                "options": [
+                    "10 alunos a mais do que possíveis respostas distintas", 
+                    "20 alunos a mais do que possíveis respostas distintas",
+                    "119 alunos a mais do que possíveis respostas distintas",
+                    "260 alunos a mais do que possíveis respostas distintas​​",
+                    "270 alunos a mais do que possíveis respostas distintas"
+                ],
+                "answer": "10 alunos a mais do que possíveis respostas distintas"
+            },
+
+            {
+                "question": "(UERJ) Uma máquina contém pequenas bolas de borracha de 10 cores diferentes, sendo 10 bolas de cada cor. Ao inserir uma moeda na máquina, uma bola é expelida ao acaso. Para garantir a retirada de 4 bolas de uma mesma cor, o menor número de moedas a serem inseridas na máquina corresponde a:",
+                "options": [
+                    "5", 
+                    "13",
+                    "31",
+                    "40"
+                ],
+                "answer": "31"
+            },
+
+            {
+                "question": "(Enem 2015) Em uma central de atendimento, cem pessoas receberam senhas numeradas de 1 até 100. Uma das senhas é sorteada ao acaso. Qual a probabilidade de a senha sorteada ser um número de 1 a 20?",
+                "options": [
+                    "1/100", 
+                    "19/100",
+                    "20/100",
+                    "21/100",
+                    "80/100"
+                ],
+                "answer": "20/100"
+            },
+
+            {
+                "question": "(Enem 2010) Em uma reserva florestal existem 263 espécies de peixes, 122 espécies de mamíferos, 93 espécies de répteis, 1 132 espécies de borboletas e 656 espécies deaves. Se uma espécie animal for capturada ao acaso, qual a probabilidade de ser uma borboleta?",
+                "options": [
+                    "63,31%", 
+                    "60,18%",
+                    "56,52%",
+                    "49,96%",
+                    "43,27%"
+                ],
+                "answer": "49,96%"
+            },
+
+            {
+                "question": "(Enem 2017 – 2ª aplicação) Um programa de televisão criou um perfil em uma rede social, e a ideia era de que esse perfil fosse sorteado para um dos seguidores, quando esses fossem em número de um milhão. Agora que essa quantidade de seguidores foi atingida, os organizadores perceberam que apenas 80% deles são realmente fãs do programa. Por conta disso, resolveram que todos os seguidores farão um teste, com perguntas objetivas referentes ao programa, e só poderão participar do sorteio aqueles que forem aprovados. Estatísticas revelam que, num teste dessa natureza, a taxa de aprovação é de 90% dos fãs e de 15% dos que não são fãs. De acordo com essas informações, a razão entre a probabilidade de que um fã seja sorteado e a probabilidade de que o sorteado seja alguém que não é fã do programa é",
+                "options": [
+                    "1", 
+                    "4",
+                    "6",
+                    "24",
+                    "96"
+                ],
+                "answer": "24"
+            },
+
+            {
+                "question": "(Enem 2017) Um morador de uma região metropolitana tem 50% de probabilidade de atrasar-se para o trabalho quando chove na região; caso não chova, sua probabilidade de atraso é de 25%. Para um determinado dia, o serviço de meteorologia estima em 30% a probabilidade da ocorrência de chuva nessa região. Qual é a probabilidade de esse morador se atrasar para o serviço no dia para o qual foi dada a estimativa de chuva?",
+                "options": [
+                    "0,075", 
+                    "0,150",
+                    "0,325",
+                    "0,600",
+                    "0,800"
+                ],
+                "answer": "0,325"
+            },
+
+            {
+                "question": "(Enem 2017) Uma aluna estuda numa turma de 40 alunos. Em um dia, essa turma foi dividida em três salas, A, B e C, de acordo com a capacidade das salas. Na sala A ficaram 10 alunos, na B, outros 12 alunos e na C, 18 alunos. Será feito um sorteio no qual, primeiro, será sorteada uma sala e, posteriormente, será sorteado um aluno dessa sala. Qual é a probabilidade de aquela aluna específica ser sorteada, sabendo que ela está na sala C?",
+                "options": [
+                    "1/3", 
+                    "1/18",
+                    "1/40",
+                    "1/54",
+                    "7/18"
+                ],
+                "answer": "1/40"
+            },
+        ]
+    },
+
+    {
+        "title": "binômio de newton (3º ano)",
+        "questions": [
+            {
+                "question": "(UNESP) Se n  é um número inteiro positivo, pelo símbolo n!  subentende-se o produto de n fatores distintos, n . (n – 1) . (n – 2) … 2 . 1. Nestas condições, qual é o algarismo das unidades do número (9!8!)7!? ",
+                "options": [
+                    "0", 
+                    "1",
+                    "2",
+                    "3",
+                    "4"
+                ],
+                "answer": "0"
+            },
+
+            {
+                "question": "(CESGRANRIO) O coeficiente de x4 no polinômio P(x) = (x + 2)6 é:",
+                "options": [
+                    "64", 
+                    "60",
+                    "12",
+                    "4",
+                    "24"
+                ],
+                "answer": "60"
+            },
+
+            {
+                "question": "A soma dos coeficientes numéricos dos termos do desenvolvimento de (x – y)104 é:",
+                "options": [
+                    "1", 
+                    "-1",
+                    "0",
+                    "104",
+                    "2"
+                ],
+                "answer": "0"
+            },
+
+            {
+                "question": "A soma dos coeficientes numéricos dos termos do desenvolvimento de (3x – 2y)n é:",
+                "options": [
+                    "1", 
+                    "-1",
+                    "2",
+                    "2n​​",
+                    "-2n"
+                ],
+                "answer": "1"
+            },
+
+            {
+                "question": "(Marinha 2020) Marque a opção que apresenta o quarto termo do binômio (3x-5)5.",
+                "options": [
+                    "-11550x2", 
+                    "-11250x2",
+                    "-11050x2",
+                    "-10050x2",
+                    "-10250x2"
+                ],
+                "answer": "-11250x2"
+            },
+
+            {
+                "question": "(Quadrix 2022) Considerando o desenvolvimento de (a + b)n, em que a e b são reais e n é natural, julgue o item. Se n = 5, então a média dos coeficientes da expansão desse binômio é maior que 5.",
+                "options": [
+                    "Certo", 
+                    "Errado"
+                ],
+                "answer": "Certo"
+            },
+
+            {
+                "question": "(Quadrix 2022) Considerando o desenvolvimento de (a + b)n, em que a e b são reais e n é natural, julgue o item. Se n = 10, então a soma de todos os coeficientes do desenvolvimento é igual a 1.024. ",
+                "options": [
+                    "Certo", 
+                    "Errado"
+                ],
+                "answer": "Certo"
+            },
+
+            {
+                "question": "Considerando o desenvolvimento de (a + b)n, em que a e b são reais e n é natural, julgue o item. Se n = 1.234, então o número de termos da expansão é um número primo. ",
+                "options": [
+                    "Certo", 
+                    "Errado"
+                ],
+                "answer": "Errado"
+            },
+
+            {
+                "question": "(IDECAN 2019) No desenvolvimento de P(x) = (ax2 − 2bx + c + 1)2 , obtenha o valor do coeficiente de maior grau sendo a = 2, b = -1 e c = 5.",
+                "options": [
+                    "2", 
+                    "4",
+                    "8",
+                    "12",
+                    "16"
+                ],
+                "answer": "4"
+            },
+
+            {
+                "question": "(IBEG 2017) No desenvolvimento de (x + 2 )n . x3 , o coeficiente de xn+1 é igual a:",
+                "options": [
+                    "n3 + 1", 
+                    "n + 2",
+                    "2n(n - 1)",
+                    "n2 (n + 1)",
+                    "n2 - 3n + 2"
+                ],
+                "answer": "2n(n - 1)"
+            },
+        ]
     }
 ]
